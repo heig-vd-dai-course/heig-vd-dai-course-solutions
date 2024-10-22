300ce59ef458d48b1b78da7d53a8781c557857c3
GeraudSilvestri <silvestri.geraud@gmail.com>
2024-10-22 10:18:19 +0200

    Creating base project with maven wrapper

--- 13-java-tcp-programming/TCPGuessTheNumber/src/main/java/org/example/TCPCLient.java
+++ 13-java-tcp-programming/TCPGuessTheNumber/src/main/java/org/example/TCPCLient.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class TCPCLient {
+    final private String HOST;
+    final private int PORT;
+
+    public TCPCLient(String HOST, int PORT) {
+        this.HOST = HOST;
+        this.PORT = PORT;
+    }
 }
