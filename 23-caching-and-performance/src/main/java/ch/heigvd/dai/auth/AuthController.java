6c8417e67c3855fae1ad2afd2902fdaf6771a080
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2024-12-13 15:19:39 +0100

    Add solution for chapter "Caching and performance" (#25)

--- 23-caching-and-performance/src/main/java/ch/heigvd/dai/auth/AuthController.java
+++ 23-caching-and-performance/src/main/java/ch/heigvd/dai/auth/AuthController.java
@@ -2,13 +2,19 @@ package ch.heigvd.dai.auth;
 
 import ch.heigvd.dai.users.User;
 import io.javalin.http.*;
+import java.time.LocalDateTime;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class AuthController {
   private final ConcurrentHashMap<Integer, User> users;
 
-  public AuthController(ConcurrentHashMap<Integer, User> users) {
+  private final ConcurrentHashMap<Integer, LocalDateTime> usersCache;
+
+  public AuthController(
+      ConcurrentHashMap<Integer, User> users,
+      ConcurrentHashMap<Integer, LocalDateTime> usersCache) {
     this.users = users;
+    this.usersCache = usersCache;
   }
 
   public void login(Context ctx) {
@@ -21,7 +27,6 @@ public class AuthController {
     for (User user : users.values()) {
       if (user.email.equalsIgnoreCase(loginUser.email)
           && user.password.equals(loginUser.password)) {
-        ctx.cookie("user", String.valueOf(user.id));
         ctx.status(HttpStatus.NO_CONTENT);
         return;
       }
@@ -44,12 +49,33 @@ public class AuthController {
 
     Integer userId = Integer.parseInt(userIdCookie);
 
+    // Get the last known modification date of the user
+    LocalDateTime lastKnownModification =
+        ctx.headerAsClass("If-Modified-Since", LocalDateTime.class).getOrDefault(null);
+
+    // Check if the user has been modified since the last known modification date
+    if (lastKnownModification != null && usersCache.get(userId).equals(lastKnownModification)) {
+      throw new NotModifiedResponse();
+    }
+
     User user = users.get(userId);
 
     if (user == null) {
       throw new UnauthorizedResponse();
     }
 
+    LocalDateTime now;
+    if (usersCache.containsKey(user.id)) {
+      // If it is already in the cache, get the last modification date
+      now = usersCache.get(user.id);
+    } else {
+      // Otherwise, set to the current date
+      now = LocalDateTime.now();
+      usersCache.put(user.id, now);
+    }
+
+    // Add the last modification date to the response
+    ctx.header("Last-Modified", String.valueOf(now));
     ctx.json(user);
   }
 }
