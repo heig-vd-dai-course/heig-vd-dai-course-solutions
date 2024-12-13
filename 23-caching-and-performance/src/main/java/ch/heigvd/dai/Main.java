79c55e3a135da46ccc5eade1705bffb4d6961575
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2024-12-13 15:43:22 +0100

    Fix port

--- 23-caching-and-performance/src/main/java/ch/heigvd/dai/Main.java
+++ 23-caching-and-performance/src/main/java/ch/heigvd/dai/Main.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
-  public static final int PORT = 8081;
+  public static final int PORT = 8080;
 
   public static void main(String[] args) {
     Javalin app =
